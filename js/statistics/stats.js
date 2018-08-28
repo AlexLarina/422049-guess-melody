b1f13a027ef3f468e740251c48ac50bc9642232c
Alexandra Larina <aa.larina.ff@gmail.com>
2018-08-28 13:56:37 +0300

    Improved showGameResult function logic

--- js/statistics/stats.js
+++ js/statistics/stats.js
@@ -14,25 +14,20 @@ const countPoints = (answers) => {
 };
 
 const showGameResult = (playersResultsArray, individualResult) => {
-  let message = ``;
-  let sortedResultArray = [];
   if (individualResult.remainTime === 0) {
     return `Время вышло! Вы не успели отгадать все мелодии`;
   } else if (individualResult.remainNotes === 0) {
     return `У вас закончились все попытки. Ничего, повезёт в следующий раз!`;
   }
-  if (individualResult.points) {
-    playersResultsArray.push(individualResult.points);
-    sortedResultArray = playersResultsArray.sort((a, b) => {
-      return a - b;
-    });
-    const playerResultIndex = sortedResultArray.indexOf(individualResult.points);
-    message = `Вы заняли ` + playerResultIndex + ` место из `
+
+  playersResultsArray.push(individualResult.points);
+  let sortedResultArray = playersResultsArray.sort((a, b) => {
+    return a - b;
+  });
+  const playerResultIndex = sortedResultArray.indexOf(individualResult.points);
+  return `Вы заняли ` + playerResultIndex + ` место из `
       + sortedResultArray.length + ` игроков. Это лучше, чем у `
       + playerResultIndex * 100 / sortedResultArray.length + `% игроков.`;
-  }
-
-  return message;
 };
 
 export {countPoints, showGameResult};
